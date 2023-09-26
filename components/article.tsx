b86aac02ee88188e733203a6a518f73991312388
Aruseli <llingveri@gmail.com>
2023-09-27 00:35:10 +0300

    article

--- components/article.tsx
+++ components/article.tsx
@@ -30,7 +30,7 @@ export const Article = React.memo<any>(({
           padding: '0.05rem',
           position: 'relative',
           borderRadius: '0.3rem',
-          background: 'linear-gradient(180deg, rgba(247,105,255,1) 0%, rgba(100,100,206,1) 35%, rgba(59,190,255,1) 100%)',
+          background: colorMode === 'dark' ? 'linear-gradient(180deg, rgba(247,105,255,1) 0%, rgba(100,100,206,1) 35%, rgba(59,190,255,1) 100%)' : 'linear-gradient(120deg, rgba(238,173,19,1) 0%, rgba(255,225,168,1) 35%, rgba(238,166,58,1) 100%)',
         }}
         {...props}
       >
