79c9b62ede09caa0f81b3ca8e9b3f4ad0694b02a
Aruseli <llingveri@gmail.com>
2023-09-30 12:29:21 +0300

    marks

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
-import theme from '../imports/theme';
+import themeChakra from '../imports/theme';
 
 
 export default function Document() {
@@ -8,17 +8,13 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" />
         <link rel="android-chrome-192x192" href="/android-chrome-192x192.png" type="image/png" sizes='192x192' />
         <link rel="android-chrome-512x512" href="/android-chrome-512x512.png" type="image/png" sizes='512x512' />
         <link rel="icon" href="/favicon.ico" />
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-DC5RRWLRNV"
-        />*/}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -29,11 +25,9 @@ export default function Document() {
             `,
           }}
         /> 
-        {/* <script src="https://gftruj.github.io/hand.tracking.controls.extras/dist/aframe-hand-tracking-controls-extras.js"></script>
-        <script src="https://gftruj.github.io/hand.tracking.controls.extras/components/dist/hand-tracking-controls-extras-components.js"> </script> */}
       </Head>
       <body>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ColorModeScript initialColorMode={themeChakra.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
